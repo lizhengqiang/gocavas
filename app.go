ecc8c3504dc6332a548def457b2f40468ba6fa04
timeloveboy <734991033@qq.com>
2016-08-01 18:31:12 +0800

    ~

--- app.go
+++ app.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"code.aliyun.com/timeloveboy/gocavas/drawing"
+	"github.com/mengxiaozhu/gocavas/drawing"
 	"golang.org/x/image/font"
 
 	"os"
@@ -14,7 +14,8 @@ import (
 
 func main() {
 
-	textimg:=drawing.DrawString(300,200,"我是李鹏","static/张海山锐线体简1.0.ttf",font.HintingNone,50,color.RGBA{0,0,255,255},100,0,0)
+	img_line1:=drawing.DrawString(300,200,"我是李鹏","static/张海山锐线体简1.0.ttf",font.HintingNone,30,color.RGBA{0,0,255,255},100,0,0)
+	img_line2:=drawing.DrawString(300,200,"我是韩寒","static/张海山锐线体简1.0.ttf",font.HintingNone,25,color.RGBA{0,255,0,255},100,0,0)
 
 	f,err:=os.Open("img/hh.jpeg")
 	defer f.Close()
@@ -24,7 +25,8 @@ func main() {
 		panic(err)
 	}
 	g:=drawing.NewFromImage(scrimg)
-	g.DrawPicture(textimg,[2]int{0,0})
+	g.DrawPicture(img_line1,[2]int{0,0})
+	g.DrawPicture(img_line2,[2]int{0,40})
 	bw:=g.SaveJpg(100)
 	ioutil.WriteFile("img/result.jpg",bw,os.ModePerm)
 	fmt.Println(len(bw))
